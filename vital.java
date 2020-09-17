74b334dffe3e245b4acbc9af5eb629872f622eb1
Vitalis5501 <ayoguvitaliskid97@gmil.com>
2020-09-17 07:37:03 -0400

    my java applet

---
+++ vital.java
@@ -0,0 +1,11 @@
+
+public class vital {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" click here");
+		System.out.println(" click to quit\n thanks ");
+
+	}
+
+}
