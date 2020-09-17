479ffc9c49677c7814efcf4a0bb9f3a0305003f5
Vitalis5501 <70792865+Vitalis5501@users.noreply.github.com>
2020-09-17 10:58:32 -0400

    Update vital.java

--- vital.java
+++ vital.java
@@ -5,6 +5,7 @@ public class vital {
 		// TODO Auto-generated method stub
 		System.out.println(" click here");
 		System.out.println(" click to quit\n thanks ");
+		System.out.println(" verrsion 2.4");
 
 	}
 
